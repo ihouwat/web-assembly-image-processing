022681b49d51d124517b85acb40739c8403be3d6
Igor <houwat@gmail.com>
2022-05-22 14:20:37 -0400

    Configure web assembly and Rust with webpack

--- public/main.js
+++ public/main.js
@@ -1,4 +1,18 @@
-function init() {
+// Importing WA file. Since importing a file is an async operation, need async keyword
+async function init() {
+
+  // Importing WA file here
+  let rustApp = null;
+  
+  try {
+    rustApp = await import('../pkg'); // pkg directory created by web assembly tools plugin
+  } catch(e) {
+    console.error(e);
+    return;
+  }
+
+  console.log(rustApp);
+
   const input = document.getElementById('upload');
   const fileReader = new FileReader();
 
